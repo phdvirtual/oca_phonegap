acb97f00085a149b9bde03502803d0d74c4b7eec
Marcelo Gomes <chelogui@gmail.com>
2014-09-03 21:34:46 -0300

    Alteracoes

--- www/js/localizations.js
+++ www/js/localizations.js
@@ -27,80 +27,78 @@ var app = {
         var image_revendedor = "img/revendedor.png";
         var marker, i;
 
-        // function getPosition(position) {
-        //     $('.loading').addClass('hide');
-        //     var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        //     my_lat = position.coords.latitude;
-        //     my_long = position.coords.longitude;
-        //
-        //     var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        //       zoom: 14,
-        //       center: new google.maps.LatLng(my_lat, my_long),
-        //       scrollwheel: true,
-        //       scaleControl: false,
-        //       mapTypeId: google.maps.MapTypeId.ROADMAP
-        //     });
-        //
-        //     marker2 = new google.maps.Marker({
-        //       position: new google.maps.LatLng(my_lat,my_long),
-        //       icon: image2,
-        //       map: map,
-        //       zIndex: Math.round(my_lat * -100000) << 5
-        //     });
-        //
-        //     google.maps.event.addListener(marker2, "click", (function( marker2, i ) {
-        //       return function(){
-        //         infowindow.setContent("<div class='box-map-voce'>" + "Este é você" + "</div>");
-        //         infowindow.open(map, marker2);
-        //       }
-        //     })(marker2, i));
-        //
-        //     var infowindow = new google.maps.InfoWindow({
-        //         size: new google.maps.Size(150, 50)
-        //     });
-        //
-        //     google.maps.event.addListener(map, 'click', function() {
-        //       infowindow.close();
-        //     });
-        //
-        //
-        //     for (i = 0; i < locations.length; i++) {
-        //
-        //       if (locations[i]['category_id'] == '1') {
-        //         current_image = image_distribuidor
-        //       } else {
-        //         current_image = image_revendedor
-        //       };
-        //       console.log(current_image);
-        //
-        //       marker = new google.maps.Marker({
-        //         position: new google.maps.LatLng(locations[i]['lat'], locations[i]['long']),
-        //         icon: current_image,
-        //         map: map,
-        //         zIndex: Math.round(locations[i][0] * -100000) << 5
-        //       });
-        //
-        //       //marker.setAnimation(google.maps.Animation.BOUNCE);
-        //
-        //       google.maps.event.addListener(marker, "click", (function( marker, i ) {
-        //         return function(){
-        //           infowindow.setContent("<div class='box-map'><div class='name'>" + locations[i]['title'] + "</div><div class='site'>" + locations[i]['phone1'] + "<br />" + locations[i]['address'] + "</div>" + "</div>");
-        //           infowindow.open(map, marker);
-        //         }
-        //       })(marker, i));
-        //
-        //     }
-        //
-        // };// getCurrentPosition
-        navigator.geolocation.getCurrentPosition(function(position){
-          my_lat = position.coords.latitude;
-          my_long = position.coords.longitude;
-          alert(my_lat);
-        });
+        navigator.geolocation.getCurrentPosition(
+          function(posicao) {
+              alert('Fabiana...');
+              var pos = new google.maps.LatLng(posicao.coords.latitude, posicao.coords.longitude);
+              my_lat = posicao.coords.latitude;
+              my_long = posicao.coords.longitude;
+
+              $('.loading').addClass('hide');
+              var map = new google.maps.Map(document.getElementById('map-canvas'), {
+                zoom: 14,
+                center: new google.maps.LatLng(my_lat, my_long),
+                scrollwheel: true,
+                scaleControl: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+              });
+
+              marker2 = new google.maps.Marker({
+                position: new google.maps.LatLng(my_lat,my_long),
+                icon: image2,
+                map: map,
+                zIndex: Math.round(my_lat * -100000) << 5
+              });
+
+              google.maps.event.addListener(marker2, "click", (function( marker2, i ) {
+                return function(){
+                  infowindow.setContent("<div class='box-map-voce'>" + "Este é você" + "</div>");
+                  infowindow.open(map, marker2);
+                }
+              })(marker2, i));
+
+              var infowindow = new google.maps.InfoWindow({
+                  size: new google.maps.Size(150, 50)
+              });
+
+              google.maps.event.addListener(map, 'click', function() {
+                infowindow.close();
+              });
+
+
+              for (i = 0; i < locations.length; i++) {
+
+                if (locations[i]['category_id'] == '1') {
+                  current_image = image_distribuidor
+                } else {
+                  current_image = image_revendedor
+                };
+                console.log(current_image);
+
+                marker = new google.maps.Marker({
+                  position: new google.maps.LatLng(locations[i]['lat'], locations[i]['long']),
+                  icon: current_image,
+                  map: map,
+                  zIndex: Math.round(locations[i][0] * -100000) << 5
+                });
+
+                //marker.setAnimation(google.maps.Animation.BOUNCE);
+
+                google.maps.event.addListener(marker, "click", (function( marker, i ) {
+                  return function(){
+                    infowindow.setContent("<div class='box-map'><div class='name'>" + locations[i]['title'] + "</div><div class='site'>" + locations[i]['phone1'] + "<br />" + locations[i]['address'] + "</div>" + "</div>");
+                    infowindow.open(map, marker);
+                  }
+                })(marker, i));
+
+              }
+
+          }// getCurrentPosition
+        );
 
       })
       .done(function(){
-        console.log('Parabéns. A requisição foi feita com sucesso.')
+        console.log('Parabzéns. A requisição foi feita com sucesso.')
       })
       .fail(function(){
         console.log('Desculpe. Algo saiu errado. Verifique sua conexão com a internet.')
