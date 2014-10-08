0a71dcd43cae617ff87258bc034ee7c941f0ae6a
Marcelo Gomes <chelogui@gmail.com>
2014-10-08 03:43:48 -0300

    Angular funcionando

---
+++ www/app/app.js
@@ -0,0 +1,34 @@
+
+var AppOca = angular.module("AppOca", ['ngRoute']);
+
+AppOca.config(function($routeProvider, $locationProvider)
+{
+ 
+   $routeProvider
+   // para a rota '/', carregaremos o template home.html e o controller 'HomeCtrl'
+   .when('/', {
+      templateUrl : 'app/views/mapa.html',
+      controller     : 'MapaCtrl',
+   })
+   .when('/lanchar', {
+      templateUrl : 'app/views/lanchar.html',
+      controller     : 'LancharCtrl',
+   })
+   .otherwise ({ redirectTo: '/' });
+
+   // remove o # da url
+   $locationProvider.html5Mode(true);
+});
+
+// AppOca.service('sharedProperties', function () {
+//   var has_data = false;
+//
+//   return {
+//       getData: function () {
+//           return has_data;
+//       },
+//       setProperty: function(value) {
+//           property = value;
+//       }
+//   };
+// });
---
+++ www/app/controllers/lancharCtrl.js
@@ -0,0 +1,7 @@
+AppOca.controller('LancharCtrl', function($rootScope, $location) {
+   $rootScope.activetab = $location.path();
+
+   $rootScope.test = "Testando";
+});
+
+// <h2 class="main-title">Próximos de mim</h2>
