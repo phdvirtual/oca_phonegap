73bb79fd465308530845fc5e974ac36dcd4a3d98
Marcelo Gomes <chelogui@gmail.com>
2014-10-10 03:01:53 -0300

    Adicionei o on device ready

--- www/app/app.js
+++ www/app/app.js
@@ -1,42 +1,45 @@
+document.addEventListener("deviceready", function() {
 
-var AppOca = angular.module("AppOca", ['ngRoute']);
+  var AppOca = angular.module("AppOca", ['ngRoute']);
 
-AppOca.config(function($routeProvider, $locationProvider)
-{
- 
-   $routeProvider
-   // para a rota '/', carregaremos o template home.html e o controller 'HomeCtrl'
-   .when('/', {
-      templateUrl : 'app/views/mapa.html',
-      controller     : 'MapaCtrl',
-   })
-   .when('/lanchar', {
-      templateUrl : 'app/views/lanchar.html',
-      controller     : 'LancharCtrl',
-   })
-   .when('/conveniencia', {
-      templateUrl : 'app/views/conveniencia.html',
-      controller     : 'ConvenienciaCtrl',
-   })
-   .when('/distribuidor', {
-      templateUrl : 'app/views/distribuidor.html',
-      controller     : 'DistribuidorCtrl',
-   })
-   .otherwise ({ redirectTo: '/' });
+  AppOca.config(function($routeProvider, $locationProvider)
+  {
+   
+     $routeProvider
+     // para a rota '/', carregaremos o template home.html e o controller 'HomeCtrl'
+     .when('/', {
+        templateUrl : 'app/views/mapa.html',
+        controller     : 'MapaCtrl',
+     })
+     .when('/lanchar', {
+        templateUrl : 'app/views/lanchar.html',
+        controller     : 'LancharCtrl',
+     })
+     .when('/conveniencia', {
+        templateUrl : 'app/views/conveniencia.html',
+        controller     : 'ConvenienciaCtrl',
+     })
+     .when('/distribuidor', {
+        templateUrl : 'app/views/distribuidor.html',
+        controller     : 'DistribuidorCtrl',
+     })
+     .otherwise ({ redirectTo: '/' });
 
-   // remove o # da url
-   $locationProvider.html5Mode(true);
-});
+     // remove o # da url
+     $locationProvider.html5Mode(true);
+  });
 
-// AppOca.service('sharedProperties', function () {
-//   var has_data = false;
-//
-//   return {
-//       getData: function () {
-//           return has_data;
-//       },
-//       setProperty: function(value) {
-//           property = value;
-//       }
-//   };
-// });
+  // AppOca.service('sharedProperties', function () {
+  //   var has_data = false;
+  //
+  //   return {
+  //       getData: function () {
+  //           return has_data;
+  //       },
+  //       setProperty: function(value) {
+  //           property = value;
+  //       }
+  //   };
+  // });
+
+}, false);
