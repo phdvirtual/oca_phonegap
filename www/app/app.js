44b63c3d9071256d42a6684f621c28cfa85e8b5c
Marcelo Gomes <chelogui@gmail.com>
2014-11-06 22:17:48 -0300

    Imagens das lojas aparecendo e logo quando nao tem

--- www/app/app.js
+++ www/app/app.js
@@ -1,1 +1,15 @@
-var AppOca = angular.module("AppOca", []);
+var AppOca = angular.module("AppOca", [])
+  .filter('numberFixedLen', function () {
+    return function (n, len) {
+      var num = parseInt(n, 10);
+      len = parseInt(len, 10);
+      if (isNaN(num) || isNaN(len)) {
+        return n;
+      }
+      num = ''+num;
+      while (num.length < len) {
+        num = '0'+num;
+      }
+      return num;
+    };
+  });
